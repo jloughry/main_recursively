0c4ab26f829256dc9cddf67c04d31a716c0e94e4
Joe Loughry <joe.loughry@gmail.com>
2017-08-05 21:05:50 -0600

    Use realloc() to avoid having to walk argv[] twice.

--- call_main_recursively.c
+++ call_main_recursively.c
@@ -7,48 +7,54 @@ char * display_argv(int argc, char ** argv);
 
 int main(int argc, char ** argv) {
     char * free_this = NULL;
+
+    // All this extra complication just to cram all of argv into a string.
     printf("main() called with %s; the value of global = %d\n",
         free_this = display_argv(argc, argv), ++global);
+
+    // The caller is responsible for freeing the string.
     free(free_this);
+
+    // Try calling main() recursively (but not infinitely).
     if (argc > 1) {
         main(argc - 1, argv);
     }
+
     printf("returning from main(); global = %d\n", global);
     return EXIT_SUCCESS;
 }
 
+// Return a string containing everything in argv (the caller must free it).
 char * display_argv(int argc, char ** argv) {
     char * s = NULL;
-    size_t total_length_of_arguments = 0;
 
     if (argc < 2) {
         char no_arguments[] = "no arguments";
 
         s = malloc(1 + strlen(no_arguments));
+        if (!s) {
+            perror("malloc");
+            exit(EXIT_FAILURE);
+        }
         strcpy(s, no_arguments);
     }
     else {
-        // Figure out how much space we need to hold all the arguments in argv.
-        for (int i=1; i<argc; i++) {
-            total_length_of_arguments += strlen(argv[i]);
-            if (i < argc - 1) {
-                ++total_length_of_arguments; // Add room for a space between.
-            }
-        }
-        ++total_length_of_arguments; // Add room for the null terminator.
-
-        s = malloc(total_length_of_arguments);
+        s = malloc(1 + strlen(argv[1]));
         if (!s) {
             perror("malloc");
+            exit(EXIT_SUCCESS);
         }
-        s[0] = '\0'; // Null terminate the new string.
-
-        for(int i=1; i<argc; i++) {
-            strcat(s, argv[i]);
-            if (i < argc - 1) {
-                strcat(s, " ");
+        strcpy(s, argv[1]);
+        // All this extra complication just to avoid a single wasted space.
+        for (int i=2; i<argc; i++) {
+            s = realloc(s, 1 + strlen(s) + 1 + strlen(argv[i]));
+            if (!s) {
+                perror("realloc");
+                exit(EXIT_FAILURE);
             }
-        }
+            strcat(s, " ");
+            strcat(s, argv[i]);
+         }
     }
     return s;
 }
